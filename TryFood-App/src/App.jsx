2056b4c9dff00705ccbc97802c44a7fd416811b7
ShakebShamsi <shakebshamsi@hotmail.com>
2026-09-16 23:56:09 +0530

    FIXED: scroll restoration issue

--- TryFood-App/src/App.jsx
+++ TryFood-App/src/App.jsx
@@ -1,12 +1,15 @@
 import Navbar from './components/navbar/Navbar';
 import Footer from './components/footer/Footer';
 import { Outlet } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+
 
 
 
 const App = () => {
    return (
       <div className="app">
+         <ScrollToTop />
          <Navbar />
          <Outlet />
          <Footer />
